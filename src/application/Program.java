0c8303748cd0ace0fa6872c3718376d498c66ce5
Fabio Akira Lucci <fabiolucci@terra.com.br>
2021-03-27 10:27:48 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -19,6 +20,8 @@ public class Program {
 		System.out.println(obj);
 		
 		System.out.println(seller);*/
+		
+		Scanner sc = new Scanner(System.in);
 		
 		//Instanciando o Dao
 		//Programa nao conhece a implementacao
@@ -54,6 +57,12 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 
+	    System.out.println("=== TEST 6: seller delete ===");
+	    System.out.println("Enter id for delete test: ");
+	    int id = sc.nextInt();
+	    sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	
+		sc.close();
 	}
 }
