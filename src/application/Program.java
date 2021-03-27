e30743975e32f8c37298e2099e4a8ef862ae61db
Fabio Akira Lucci <fabiolucci@terra.com.br>
2021-03-27 09:17:33 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,19 +11,22 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-		Department obj = new Department(1, "Books");
+		/*Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 				
 		System.out.println(obj);
 		
-		System.out.println(seller);
+		System.out.println(seller);*/
 		
 		//Instanciando o Dao
 		//Programa nao conhece a implementacao
 		//Forma de faxer injecao de dependencia sem explicitar a implementacao
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 		
 	}
 
